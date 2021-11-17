68bd8a4a41eade08551d986e50b6c0fd4f2f23c2
jg932 <jared.h.goldstein@gmail.com>
2021-11-16 19:44:49 -0500

    solved

--- python-functions.py
+++ python-functions.py
@@ -1,10 +1,37 @@
 
-
+# Challenge #1
 def sum_to(n):
   sum = 0
   for n in range(1, n +1):
     sum += n
   return sum
 
-
 print(sum_to(6))
+
+
+
+# Challenge #2
+def largest(list):
+  list.sort()
+  return list[-1]
+
+print(largest([2,111,5,9,22,45,68]))
+
+
+
+# Challenge #3
+def occurances(string, string2):
+  return string.count(string2)
+
+print(occurances("This is a normal sentence", "e"))
+
+
+# Challenge #4
+
+def product(*nums):
+  product = 1
+  for num in nums:
+    product *= num
+  return product
+
+print(product(2,2,10,3))
